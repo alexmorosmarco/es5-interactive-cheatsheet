4c30886634f82baa59925ae54a34405a809a21c5
Alex <alex.moros.marco@gmail.com>
2015-11-22 02:23:07 +0100

    Xml HTTP requests refactored to a new script

---
+++ js/xhr.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Will fail cause we are trying to get a resource from a different domain
+// without CORS enabled in the server
+function xhrSyncCORSFromDifferentOrigin() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://www.gruposcoutokapi.com", false);
+  xhr.send();
+  writeToConsole(xhr.responseText);
+}
+
+function xhrSyncCORSThroughReverseProxy() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://www.corsproxy.com/www.gruposcoutokapi.com", false);
+  xhr.send();
+  writeToConsole(xhr.responseText);
+}
+
+function xhrSyncCORSThroughReverseProxyWithPreflight() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://www.corsproxy.com/www.gruposcoutokapi.com", false);
+  // Set a non-simple header to force a preflight CORS request (so using the
+  // OPTIONS HTTP Method).
+  xhr.setRequestHeader('Content-Type', 'alexxx');
+  xhr.send();
+  writeToConsole(xhr.responseText);
+}
+
+function xhrAsyncCORSThroughReverseProxyWithPreflight() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://www.corsproxy.com/www.gruposcoutokapi.com", true);
+  // Set a non-simple header to force a preflight CORS request (so using the
+  // OPTIONS HTTP Method).
+  xhr.setRequestHeader('Content-Type', 'alexxx');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      writeToConsole(xhr.responseText);
+    }
+  }
+  xhr.send();
+}
