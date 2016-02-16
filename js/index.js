d5c44907431d2861fe9ae53024ab5bcea26ea6f6
Alex <alex.moros.marco@gmail.com>
2016-02-16 14:08:44 +0100

    Google Analytics events added to track user actions

--- js/index.js
+++ js/index.js
@@ -12,10 +12,31 @@ var writeToConsole;
     writeToConsole = Browser.Feedback.writeToConsole;
     writeToConsole("timestamp=" + Date.now() + " - Loaded!!!");
     // Register events
+    // Track user actions
+    var buttons = document.getElementsByTagName('button');
+    var i;
+    for (i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function(event) {
+        var buttonText = this.textContent;
+        var section = this.parentElement;
+        var sectionText = section.getElementsByTagName('h4')[0].textContent;
+        var article = section.parentElement;
+        var articleText = article.getElementsByTagName('h3')[0].textContent;
+
+        ga('send', {
+          hitType: 'event',
+          eventCategory: articleText,
+          eventAction: sectionText,
+          eventLabel: buttonText
+        });
+      }, false);
+    }
+    // Register a click event dynamically
     window.document.getElementById('btUseNamespace').onclick = function() {
       Language.Namespaces.Util.log('Namespace', 'using a custom namespace...');
     };
   }
+
   document.addEventListener('DOMContentLoaded', function(event) {
     onLoaded();
   }, false);
